e30084dfb55ac7f487deba257b0dd4190cab876a
jaba9121 <160114001+jaba9121@users.noreply.github.com>
2024-04-14 11:10:25 +0300

    Update tstack.h

--- include/tstack.h
+++ include/tstack.h
@@ -2,9 +2,44 @@
 #ifndef INCLUDE_TSTACK_H_
 #define INCLUDE_TSTACK_H_
 
-template<typename T, int size>
+#include <string>
+#include <stdexcept>
+
+template <typename T, int size>
 class TStack {
-  // добавьте код стека
+ private:
+    T* data;
+    int top;
+
+ public:
+    TStack() : top(-1) {
+        data = new T[size];
+    }
+    ~TStack() {
+        delete[] data;
+    }
+    void push(T value) {
+        if (!isFull()) {
+            data[++top] = value;
+        } else {
+            throw std::overflow_error("Stack is full");
+        }
+    }
+    T get() const {
+         return data[top];
+    }
+    T pop() {
+        if (isEmpty()) {
+            throw std::underflow_error("Stack is empty");
+        }
+        return data[top--];
+    }
+    bool isEmpty() const {
+        return top == -1;
+    }
+    bool isFull() const {
+        return top == size - 1;
+    }
 };
 
-#endif  // INCLUDE_TSTACK_H_
+#endif // INCLUDE_TSTACK_H_
